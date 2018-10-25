903448af3e8be6ae2c5107437ed00c019c8350db
adriancro8888 <adriancro8888@gmail.com>
2018-10-25 12:27:18 +0530

    CanaryTest, created test file.

---
+++ APIMVITests/APIMVITests-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  APIMVITests-Bridging-Header.h
+//  APIMVI
+//
+//  Created by Dinesh IIINC on 25/10/18.
+//  Copyright © 2018 kite.work. All rights reserved.
+//
+
+#ifndef APIMVITests_Bridging_Header_h
+#define APIMVITests_Bridging_Header_h
+
+
+#endif /* APIMVITests_Bridging_Header_h */
---
+++ APIMVITests/CanaryTest.swift
@@ -0,0 +1,17 @@
+//
+//  CanaryTest.swift
+//  APIMVITests
+//
+//  Created by Dinesh IIINC on 25/10/18.
+//  Copyright © 2018 kite.work. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class CanaryTest: XCTestCase {
+
+  func canaryTest() {
+    XCTAssertEqual(true, true)
+  }
+}
