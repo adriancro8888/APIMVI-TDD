f1f0e13297987dbccc720b62ef67e9386798397e
adriancro8888 <adriancro8888@gmail.com>
2018-10-30 11:49:37 +0530

    NetworkManager, implemented network layer.

---
+++ APIMVI/Blogs/NetworkManager.swift
@@ -0,0 +1,35 @@
+//
+//  NetworkManager.swift
+//  APIMVI
+//
+//  Created by Dinesh IIINC on 29/10/18.
+//  Copyright © 2018 kite.work. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxAlamofire
+import Alamofire
+
+enum NetworkError: Error {
+  case failure
+}
+
+class NetworkManager {
+
+  func fetchBlogs() -> Observable<[Blog]> {
+    return
+      request(
+       .get,
+       "http://demo3027021.mockable.io/post",
+       parameters: nil,
+       encoding: JSONEncoding.default,
+       headers: nil
+      )
+      .validate()
+      .responseData()
+      .map({ (_, data) in
+        return try JSONDecoder().decode([Blog].self, from: data)
+      })
+  }
+}
