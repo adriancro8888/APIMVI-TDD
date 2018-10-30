8c8c3a054919fe7c4445e2cb45935158e011a088
adriancro8888 <adriancro8888@gmail.com>
2018-10-30 11:40:04 +0530

    MviLifecycle, enums for maintaining life cycle events.

---
+++ APIMVI/MviLifecycle.swift
@@ -0,0 +1,15 @@
+//
+//  MviLifecycle.swift
+//  APIMVI
+//
+//  Created by Dinesh IIINC on 26/10/18.
+//  Copyright © 2018 kite.work. All rights reserved.
+//
+
+import Foundation
+
+enum MviLifecycle {
+  case created
+  case restored
+  case stopped
+}
