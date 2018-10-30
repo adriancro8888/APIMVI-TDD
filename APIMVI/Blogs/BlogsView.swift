e6bd8fe00be67f1a40abedea539c6e7efbedba1a
adriancro8888 <adriancro8888@gmail.com>
2018-10-30 15:55:17 +0530

    Introduce BlogsView.

---
+++ APIMVI/Blogs/BlogsView.swift
@@ -0,0 +1,13 @@
+//
+//  BlogsView.swift
+//  APIMVI
+//
+//  Created by alice singh on 30/10/18.
+//  Copyright © 2018 kite.work. All rights reserved.
+//
+
+import Foundation
+
+protocol BlogsView {
+    func  render(state: BlogState)
+}
