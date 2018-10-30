bf6421b0837529ce4a19d8110608f3a9b1275f39
adriancro8888 <adriancro8888@gmail.com>
2018-10-30 11:50:30 +0530

    ViewController, add stub glue code.

--- APIMVI/ViewController.swift
+++ APIMVI/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import Foundation
+import RxSwift
+import RxCocoa
+import RxAlamofire
+import Alamofire
 
 class ViewController: UIViewController {
 
+  let disposeBag = DisposeBag()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    getRequest().subscribe(onNext: { (arr) in
+      print("Array Count")
+      print(arr.count)
+    }, onError: {
+      (error) in
+      print("Error")
+      print(error.localizedDescription)
+    }).disposed(by: disposeBag)
   }
 
+  func getRequest() -> Observable<[Blog]> {
 
+    let urlString = "http://demo3027021.mockable.io/posts"
+
+        return
+          request(
+            .get,
+            urlString,
+            parameters: nil,
+            encoding: JSONEncoding.default,
+            headers: nil
+          )
+          .validate()
+          .responseData()
+          .map({ (_, data) -> [Blog] in
+            return try JSONDecoder().decode([Blog].self, from: data)
+          })
+  }
 }
 
