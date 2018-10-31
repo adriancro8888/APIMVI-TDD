cef1ecfff7ec5327ba6aae9be2a302126cf736dd
adriancro8888 <adriancro8888@gmail.com>
2018-10-31 13:01:02 +0530

    Review Changes #3,  refactored blogsView.

--- APIMVI/Blogs/BlogsView.swift
+++ APIMVI/Blogs/BlogsView.swift
@@ -19,21 +19,29 @@ protocol BlogsView {
 
 extension BlogsView {
     func render(state: BlogState) {
-        if (state.status == .failure) {
+        switch state.status {
+            case .fetching : render(fetching: state)
+            case .failure  : render(failure: state)
+            case .success  : render(success: state)
+        }
+    }
+    
+    private func render(fetching state: BlogState) {
+        showLoading(show: true)
+        showRetry(show: false)
+    }
+    
+    private func render(failure state: BlogState) {
+        showLoading(show: false)
+        showRetry(show: true)
+    }
+    
+    private func render(success state: BlogState) {
+        if state.searchQuery == "" {
             showLoading(show: false)
-            showRetry(show: true)
-        } else if (state.status == .success) {
-            if state.searchQuery == "" {
-                showLoading(show: false)
-                showBlogs(blogs: state.allBlogs)
-            } else {
-                showBlogs(blogs: state.filteredBlogs)
-            }
+            showBlogs(blogs: state.allBlogs)
         } else {
-            showLoading(show: true)
-            showRetry(show: false)
+            showBlogs(blogs: state.filteredBlogs)
         }
     }
 }
-
-
